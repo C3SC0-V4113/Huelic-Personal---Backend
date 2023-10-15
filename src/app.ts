ea04d187036c02332d2b2696c3fe90f3a038f9af
CESCO <cescovalledev@gmail.com>
2023-10-14 21:48:33 -0600

    initializing first endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@ import express from "express";
 
 /** Crear el servidor de express */
 const app = express();
+const port = 4000;
+
+/** Rutas */
+app.get("/", (req, res) => {
+  console.log("Se requiere en /");
+  res.json({
+    ok: true,
+  });
+});
 
 /** Escuchar peticiones */
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${4000}`);
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
 });
